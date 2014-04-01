b1d170e375b163226bf089cb3cb754146a73b34c
romang <romang@tycoint.com>
2014-04-01 18:55:05 +0300

    Removed not needed views

--- menu/views/main_screen.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_SCREEN_H
-#define MAIN_SCREEN_H
-#include "../../libs/stm32f10x.h"
-#include "../../hd44780/hd44780.h"
-#include "../../kb_driver/keyboard_driver.h"
-#include "../menu.h"
-
-void main_screen(void);
-#endif
--- menu/views/set_state.h
+++
@@ -1,8 +0,0 @@
-#ifndef SET_STATE_H
-#define SET_STATE_H
-#include "../../libs/stm32f10x.h"
-
-void set_state_menu(uint8_t btn, uint8_t duration);
-
-#endif
-
